d448c449ac56e036e3a4c8bf1dea038662289bc2
Vinicius Iago <160291470+VIONE34-PNG@users.noreply.github.com>
2025-03-18 21:56:01 -0300

    Add files via upload

---
+++ jogopokemonado/src/jogopokemonado/Pokemon.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package jogopokemonado;
+
+/**
+ *
+ * @author vinic
+ */
+public interface Pokemon {
+    String getNome();
+    String getTipo();
+    int getHP();
+    int getAtaque();
+    int getDefesa();
+    int getEspecial();
+}
